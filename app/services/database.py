f25167bc8b74077e11331963440f7a2c46409021
xxfrvx <xxfrvx@gmail.com>
2025-08-12 14:24:36 +0300

    database.py

--- app/services/database.py
+++ app/services/database.py
@@ -1,16 +1,22 @@
 import asyncpg
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 class Database:
-    def __init__(self, dsn):
-        self.dsn = dsn
+    def __init__(self):
+        self.dsn = os.getenv("DATABASE_URL")
         self.pool = None
 
     async def connect(self):
+        if not self.dsn:
+            logger.error("DATABASE_URL is not set in environment variables.")
+            raise ValueError("Missing DATABASE_URL")
+
         try:
-            self.pool = await asyncpg.create_pool(dsn=self.dsn)
+            # Если Railway требует SSL — убери ssl=False
+            self.pool = await asyncpg.create_pool(dsn=self.dsn, ssl=False)
             logger.info("Database pool created")
         except Exception as e:
             logger.error("Failed to connect to DB: %s", str(e))
