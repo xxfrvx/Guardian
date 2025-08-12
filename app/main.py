f9e6deb51019b781c3926f64f6880544ca894c1e
xxfrvx <xxfrvx@gmail.com>
2025-08-12 05:53:19 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
-from telegram.ext import ApplicationBuilder
+from telegram.ext import ApplicationBuilder, Application, CallbackQueryHandler
 from app import config
 from app.handlers import start, menu, profile, check, reports, moderation, appeals, coins, groups
-from telegram.ext import Application
 from handlers.callback_terms import cb_accept
-from telegram.ext import CallbackQueryHandler
 
+import asyncio
+import logging
 
+# Обёртка для PostgreSQL
+from services.database import Database
 
-def main():
+logger = logging.getLogger(__name__)
+
+# Инициализируем объект базы как глобальный
+db = Database(dsn=config.POSTGRES_DSN)
+
+async def main():
+    await db.connect()  # Подключаемся к базе перед запуском бота
+
     app = ApplicationBuilder().token(config.BOT_TOKEN).concurrent_updates(True).build()
+
+    # Подключаем хендлер согласия
     app.add_handler(CallbackQueryHandler(cb_accept, pattern="accept_terms"))
 
+    # Устанавливаем другие хендлеры
     start.setup(app)
     menu.setup(app)
     profile.setup(app)
@@ -21,8 +33,8 @@ def main():
     coins.setup(app)
     groups.setup(app)
 
-    print(f"{config.BOT_NAME} запущен.")
-    app.run_polling(close_loop=False)
+    logger.info(f"{config.BOT_NAME} запущен.")
+    await app.run_polling(close_loop=False)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
