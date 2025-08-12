399c73224a4fafdaf43649e2fb34efc0746dd594
xxfrvx <xxfrvx@gmail.com>
2025-08-12 14:57:19 +0300

    callback_terms.py

--- app/handlers/callback_terms.py
+++ app/handlers/callback_terms.py
@@ -7,8 +7,8 @@ logger = logging.getLogger(__name__)
 async def cb_accept(update: Update, context: ContextTypes.DEFAULT_TYPE):
     q = update.callback_query
     await q.answer()
-
-    logger.info("Callback received: %s from user %s", q.data, q.from_user.id)
+    
+    logger.info("cb_accept triggered by user_id=%s", q.from_user.id)
 
     try:
         db = context.bot_data["db"]
